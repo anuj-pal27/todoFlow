f2049c322f91a25a54b81bc1809bc8e87215708d
anuj-pal27 <anujpal27669@gmail.com>
2025-07-08 17:05:57 +0530

    fixes the navigation

--- taskflow_frontend/src/App.js
+++ taskflow_frontend/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import {Routes,Route} from 'react-router-dom';
+import {Routes,Route,Navigate} from 'react-router-dom';
 
 import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
@@ -14,6 +14,11 @@ function App() {
   return (
 
         <Routes>
+          <Route path="/" element={
+            localStorage.getItem('token')
+              ? <Navigate to="/dashboard" replace />
+              : <Navigate to="/login" replace />
+          } />
           <Route path='/login' element={<LoginForm />} />
           <Route path='/signup' element={<SignupForm />} />
           <Route path='/create' element={<TaskCreate />} />
